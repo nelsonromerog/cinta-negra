7a1a64568e17c9f5344b7723d448cd87c738185c
nelsonromerog <nelsonromero219@gmail.com>
2020-06-24 20:13:18 -0700

    Resolviendo ejercicios 1, 2, 3 , 4

--- 02-API/index.js
+++ 02-API/index.js
@@ -25,6 +25,11 @@ app.get('/api/', (req, res) => {
         como el siguiente:
                         {‘resultado’: 7}
 */
+app.get('/api/suma', (req, res) => {
+    const {num1, num2} = req.query
+    const result = parseInt(num1) + parseInt(num2)
+    res.status(200).json({suma: result})
+})
 /*                        
     3.- Agrega un endpoint ‘/api/usuario/’ que responda a
         una petición de tipo GET con el nombre que sea 
@@ -32,6 +37,10 @@ app.get('/api/', (req, res) => {
         con un código de estado 200 y un json como este:
                       {‘usuario’: ‘Edwin’}
 */
+app.get('/api/usuario/:name', (req, res) => {
+    const { name } = req.params
+    res.status(200).json({usuario: name})
+})
 /*                      
     4.- Agrega un endpoint ‘/api/swapi’ que responda a una
         petición de tipo GET con el personaje solicitado de 
@@ -43,6 +52,19 @@ app.get('/api/', (req, res) => {
                         ...,
                     }}
 */
+const request = require('request')
+
+app.get('/api/swapi/:id', (req, res) => {
+    const { id } = req.params
+    const SWAPI_DEV_URL = `https://swapi.dev/api/people/${id}/`
+    request.get(SWAPI_DEV_URL, (err, resSWAPI, body) => {
+        if (resSWAPI.statusCode === 200) {
+            const result = JSON.parse(body)
+            res.status(200).json({personaje: result})
+        }
+        else console.log(res.statusCode, err);
+    })
+})
 
 //Encender la API
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`))
