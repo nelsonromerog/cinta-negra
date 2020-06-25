e1d98a2743e8eef92ff57b8c3c4ce5e22a66368c
nelsonromerog <nelsonromero219@gmail.com>
2020-06-24 17:42:59 -0700

    Crear funcion anonima

--- 01-Repaso/index.js
+++ 01-Repaso/index.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const { get } = require('request');
 
 /*
     JS Vanilla
@@ -34,22 +35,48 @@ const request = require('request');
     (Higher Order Function)
         -> funciones que reciben como argumento otra funcion
 */
-// const API_URL = ''
-// request.get(API_URL, () => {
-//     /*
-//         Esta funciuon que se ejecuta dentro de la funcion de 
-//         orden superior se conoce como CALLBACK
-//     */
 
+// API URL breaking-bad-quotes
+// const BR_BA_QUOTES = 'https://breaking-bad-quotes.herokuapp.com/v1/quotes';
+// request.get(BR_BA_QUOTES, (err, res, body) => {
+//     if (res.statusCode === 200) {
+//         const json = JSON.parse(body);
+//         console.log(json);
+//         console.log(json[0].quote);
+
+//     }
+//     else console.log(res.statusCode, err);
 // })
 
-const BR_BA_QUOTES = 'https://breaking-bad-quotes.herokuapp.com/v1/quotes';
-request.get(BR_BA_QUOTES, (err, res, body) => {
-    if (res.statusCode === 200) {
-        const json = JSON.parse(body);
-        console.log(json);
-        console.log(json[0].quote);
+// API URL the-audio-db API_KEY=1 Artist_Name=coldplay    
+const getBandInfo = () => {
+    const AUDIO_DB = 'https://theaudiodb.com/api/v1/json/1/discography.php?s=coldplay';
+    request.get(AUDIO_DB, (err, res, body) => {
+        if (res.statusCode === 200) {
+            const json = JSON.parse(body);
+            for(let k in json.album) {
+                console.log('Album: ' + json.album[k].strAlbum + ' Year: ' + json.album[k].intYearReleased);
+            }
+        }
+        else console.log(res.statusCode, err);
+    })
+}
 
-    }
-    else console.log(res.statusCode, err);
-})
+getBandInfo()
+
+
+// request({
+//     method: 'get',
+//     url: 'https://api.tradier.com/v1/markets/options/strikes',
+//     qs: {
+//        'symbol': 'VXX',
+//        'expiration': '2019-05-17'
+//     },
+//     headers: {
+//       'Authorization': 'Bearer <1OmNdUOuryhraoRvTRyTAEQcMbNA>',
+//       'Accept': 'application/json'
+//     }
+//   }, (error, response, body) => {
+//       console.log(response.statusCode);
+//       console.log(body);
+//   });
