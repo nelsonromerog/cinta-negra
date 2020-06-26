e824d50d81681195816a577a16035f20309efe33
nelsonromerog <nelsonromero219@gmail.com>
2020-06-25 17:27:40 -0700

    Agregar configuracion basica de express

---
+++ 03-Base-de-Datos/index.js
@@ -0,0 +1,12 @@
+//Configuracion
+const express = require('express')
+const api = express()
+const PORT = 3000
+
+//Endpoints
+api.get('/', (_, res) => res.status(200).json({message: "it's alive!"}))
+
+
+//Encender el servidor
+api.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`))
+
