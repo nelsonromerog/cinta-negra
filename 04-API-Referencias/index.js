ea54793e52d1850a2b7443dde1954371c59ff84f
nelsonromerog <nelsonromero219@gmail.com>
2020-06-30 16:52:53 -0700

    Actualizando index.js principal

--- 04-API-Referencias/index.js
+++ 04-API-Referencias/index.js
@@ -1,2 +1,15 @@
-require('dotenv').config()
-require('./mongoClient/index')
+require('dotenv').config();
+require('./mongoClient/index.js');
+const express = require('express');
+const api = express();
+const PORT = process.env.PORT || 3000;
+
+api.use(express.urlencoded({ extended: true }));
+api.use(express.json({ extended: true }));
+
+api.get('/', (req, res) => res.json({ message: "It's alive!" }));
+
+// CRUD Products
+api.use(require('./routes/ProductRoutes'));
+
+api.listen(PORT, () => console.log(`Listening on ${PORT}`));
